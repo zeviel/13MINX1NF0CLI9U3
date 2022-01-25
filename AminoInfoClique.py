bfa344c394fca9dfcb99ba648856b44138f6fa73
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-01-25 13:00:59 +0600

    Update AminoInfoClique.py

--- AminoInfoClique.py
+++ AminoInfoClique.py
@@ -1,12 +1,12 @@
-import pyfiglet
-from configs import main_functions, menu_configs
 from tabulate import tabulate
+from pyfiglet import figlet_format
+from configs import main_functions, menu_configs
 print("""Script by DeLuvSushi
 Github : https://github.com/deluvsushi""")
-print(pyfiglet.figlet_format("aminoinfoclique", font="chunky", width=58))
+print(figlet_format("aminoinfoclique", font="chunky", width=58))
 print(tabulate(menu_configs.main_menu, tablefmt="github"))
-select = input("Select >> ")
+select = int(input("-- Select::: ")
 
-if select == "1":	main_functions.get_global_user_info()
-elif select == "2":	main_functions.get_chat_info()
-elif select == "3":	main_functions.get_account_info()
+if select == 1:	main_functions.get_global_user_info()
+elif select == 2:	main_functions.get_chat_info()
+elif select == 3:	main_functions.get_account_info()
