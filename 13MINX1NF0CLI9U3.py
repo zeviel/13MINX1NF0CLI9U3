3116dfb332d47c00de0566cf3c534f4a0df42ad2
zeviel <77536370+zeviel@users.noreply.github.com>
2022-08-25 15:36:17 +0800

    Update 13MINX1NF0CLI9U3.py

--- 13MINX1NF0CLI9U3.py
+++ 13MINX1NF0CLI9U3.py
@@ -1,12 +1,17 @@
 from tabulate import tabulate
 from pyfiglet import figlet_format
 from configs import main_functions, menu_configs
-print("""Script by DeLuvSushi
-Github : https://github.com/deluvsushi""")
+print("""
+Script by zeviel
+Github : https://github.com/zeviel
+""")
 print(figlet_format("13MINX1NF0CLI9U3", font="chunky", width=58))
 print(tabulate(menu_configs.main_menu, tablefmt="github"))
 select = int(input("-- Select::: "))
 
-if select == 1:	main_functions.get_global_user_info()
-elif select == 2:	main_functions.get_chat_info()
-elif select == 3:	main_functions.get_account_info()
+if select == 1:	
+  main_functions.get_global_user_info()
+elif select == 2:
+  main_functions.get_chat_info()
+elif select == 3:
+  main_functions.get_account_info()
