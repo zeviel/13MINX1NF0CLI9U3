534eaa2a08e86a1014e1bf173b203db663b0b18a
zeviel <77536370+zeviel@users.noreply.github.com>
2022-09-01 18:51:28 +0500

    Update and rename configs/menu_configs.py to utils/configs.py

--- configs/menu_configs.py
+++
@@ -1,6 +0,0 @@
-main_menu = [
-    ["Menu", "AminoInfoClique"],
-    ["1", "Get Global User Info"],
-    ["2", "Get Chat Info"],
-    ["3", "Get Account Info"]
-]
---
+++ utils/configs.py
@@ -0,0 +1,18 @@
+MAIN_MENU = [
+    ["Menu", "13MINX1NF0CLI9U3"],
+    [1, "retrieve user info"],
+    [2, "retrieve chat info"],
+    [3, "retrieve account info"]
+]
+
+LOGO = """ 
+\033[38;5;73m                                                                                                                                                                                  
+ #  ### # # ### ### # #  #  ### ### ###  ## #   ### ### # # ### 
+##    # ###  #  # # # # ##  # # #   # # #   #    #  # # # #   # 
+ #   ## ###  #  # #  #   #  # # ##  # # #   #    #  ### # #  ## 
+ #    # # #  #  # # # #  #  # # #   # # #   #    #    # # #   # 
+### ### # # ### # # # # ### # # #   ###  ## ### ### ### ### ### 
+[Author]::: zeviel
+[GitHub]::: https://github.com/zeviel 
+"""
+
